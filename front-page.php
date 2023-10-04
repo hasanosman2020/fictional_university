a6b491f77a7f4b6ab194e6f32532a08e8128b16a
hasanosman2020 <hasan79.hoa@gmail.com>
2023-10-04 02:29:24 +0100

    feat: manipulating default URL based queries tweaking all events page

--- front-page.php
+++ front-page.php
@@ -15,9 +15,22 @@ get_header();
       <div class="full-width-split__one">
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
-          <?php $homepageEvents = new WP_Query(array(
+
+          <?php 
+          $today = date('Ymd');
+          $homepageEvents = new WP_Query(array(
             'posts_per_page' => 2,
-            'post_type' => 'event'
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today
+              )
+            )
           ));
           while($homepageEvents->have_posts()){
             $homepageEvents->the_post(); ?>
