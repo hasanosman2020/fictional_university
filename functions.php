b39f2e39860cf047fe3d4c04e665aaba79ea56a0
hasanosman2020 <hasan79.hoa@gmail.com>
2023-10-13 03:53:22 +0100

    Create campus post type and custom field for google map. BUG: google map does not load properly.

--- functions.php
+++ functions.php
@@ -76,3 +76,10 @@ function university_adjust_queries($query){
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+function universityMapKey($api){
+    $api['key'] = 'AIzaSyCWygLjtengIlQnV084Ct_b3FsrD2J8H8Y';
+    return $api;
+}
+
+add_filter('acf/fields/google_map/api', 'universityMapKey');
