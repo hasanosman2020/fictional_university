15bbb4466308d18e69d3fc6c226cef8bda7eae3e
hasanosman2020 <hasan79.hoa@gmail.com>
2023-10-16 13:40:24 +0100

    Create templte for single campus link programmes and campuses

--- single-campus.php
+++ single-campus.php
@@ -9,49 +9,54 @@ while (have_posts()){
      <div class="container container--narrow page-section">
        <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('programme'); ?>"><i class="fa fa-home" aria-hidden="true"></i>All Programmes</a> <span class="metabox-main"><?php the_title(); ?></span>
+          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('campus'); ?>"><i class="fa fa-home" aria-hidden="true"></i>All Campuses</a> <span class="metabox-main"><?php the_title(); ?></span>
         </p>
       </div>
       
         <div class="generic-content">
-            <?php the_content(); ?>
-        </div>
+            <?php the_content();
+} 
+            ?> 
+            </div>
+            <?php $mapLocation = get_field('map_location');
+            ?>
+       
+       <div class="acf-map">
+        <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>"><h3><?php the_title(); ?>
+    <?php echo $mapLocation['address']; ?>
+        </h3></div>
+</div>
 
         <?php
-        $relatedProfessors = new WP_Query(array(
+        $relatedProgrammes = new WP_Query(array(
             'posts_per_page' => -1,
-            'post_type' => 'professor',
+            'post_type' => 'programme',
             'orderby' => 'title',
             'order' => 'ASC',
             'meta_query' => array(
               array(
-                'key' => 'related_programme',
+                'key' => 'related_campus',
                 'compare' => 'LIKE',
                 'value' => '"'.get_the_ID().'"'
               )
             )
           ));
 
-          if($relatedProfessors->have_posts()){
+          if($relatedProgrammes->have_posts()){
           echo '<hr class="section-break">';
-          echo '<h2 class="headline headline--medium"> '.get_the_title().' Professor(s)</h2>';
+          echo '<h2 class="headline headline--medium"> Programmes </h2>';
           
-          while($relatedProfessors->have_posts()){
-            $relatedProfessors->the_post(); ?>
-            <li class="professor-card__list-item">
-              <a class="professor-card" href="<?php the_permalink(); ?>">
-              <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>">
-              <span class="professor-card__name"><?php the_title(); ?></span>
-          </a>
-          </li>
-
-            </li>
-          <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+      echo '<ul class="link-list min-list">';
           
-          <?php }
-          echo '</ul>';
-              }
+          while($relatedProgrammes->have_posts()){
+            $relatedProgrammes->the_post(); ?>
+            <li>
+              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
           
+          
+        <?php  }
+         echo '</ul>';
+          }
 wp_reset_postdata();
 
           $today = date('Ymd');
@@ -83,28 +88,12 @@ wp_reset_postdata();
           while($homepageEvents->have_posts()){
             $homepageEvents->the_post(); 
             get_template_part('template-parts/content-event');
-            
-};
-};
+          };
+        };
+        wp_reset_postdata();
 
-wp_reset_postdata();
-
-$relatedCampuses = get_field('related_campus');
-if($relatedCampuses){
-  echo '<hr class="section-break">';
-  echo '<h2 class="headline headline--medium">' . get_the_title() . ' is also available at these campuses:</h2>';
-
-  echo '<ul class="min-list link-list">';
-  foreach($relatedCampuses as $campus){ ?>
-  <li><a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus); ?></a></li>
-  <?php
-}
-echo '</ul>';
-}
 ?>
-    </div>
-
-    <?php };
+</div>
+    <?php 
 get_footer();
 ?>
-
