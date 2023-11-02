2c1fbe68f2f97c9cd26ff130c33c3dd8b5422a61
hasanosman2020 <hasan79.hoa@gmail.com>
2023-11-02 01:56:08 +0000

    create own raw json data

---
+++ includes/search-route.php
@@ -0,0 +1,27 @@
+<?php
+function universityRegisterSearch(){
+    register_rest_route('university/v1', 'search', array(
+        'methods' => WP_REST_SERVER::READABLE,
+        'callback' => 'universitySearchResults'
+    ));
+};
+function universitySearchResults(){
+    //return 'Congrats, you created a route.';
+    $professors = new WP_QUERY(array(
+        'post_type' => 'professor'
+    ));
+    $professorResults = array();
+    while($professors->have_posts()){
+        $professors->the_post();
+        array_push($professorResults, array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+        ));
+    };
+    return $professorResults;
+};
+
+
+add_action('rest_api_init', 'universityRegisterSearch');
+
+?>
