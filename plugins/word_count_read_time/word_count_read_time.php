5838babab8ed67bcf851e1bcb81e14d891e47318
hasanosman2020 <hasan79.hoa@gmail.com>
2024-04-01 10:59:24 +0100

    Create the settings page for the wordcount plugin and create the first field in the html form of "Display Location"

--- plugins/word_count_read_time/word_count_read_time.php
+++ plugins/word_count_read_time/word_count_read_time.php
@@ -9,14 +9,36 @@ Author URI: https://www.hasanosman2020.github.com
  class WordCountReadTime{
     function __construct(){
         add_action('admin_menu', array($this, 'adminPage' ));
+        add_action('admin_init', array($this, 'settings'));
 
     }
+    function settings(){
+add_settings_section('wcp_first_section', null, null, 'word-count-read-time-settings-page');
+
+        add_settings_field('wcp_location', 'Display Location',array($this, 'locationHTML'), 'word-count-read-time-settings-page', 'wcp_first_section');
+
+        register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0')); 
+    }
+
+    function locationHTML(){ ;?>
+    <select name='wcp_location'>
+    <option value='0'>Beginning of post</option>
+    <option value = '1'>End of post</option>
+    </select>
+<?php }
     function adminPage(){
     add_options_page('Word Count Read Time Settings','Word Count Read Time', 'manage_options', 'word-count-read-time-settings-page', array($this,'ourHTML'));
 }
 function ourHTML(){?>
 <div class="wrap">
 <h1>Word Count Read Time Settings Page</h1>
+<form action="options.php" method="POST">
+    <?php
+    settings_fields('wordcountplugin');
+    do_settings_sections('word-count-read-time-settings-page');
+    submit_button();
+    ?>
+</form>
 </div>
 <?php
 }
