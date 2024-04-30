3d213f99994eb0fdfb29ab660fc2dd53e7ac3e09
hasanosman2020 <hasan79.hoa@gmail.com>
2024-04-30 13:29:36 +0100

    Complete functionality for second plugin word filter
    BUG: fatal error in template.php line 1712

--- plugins/word_filter_plugin/index.php
+++ plugins/word_filter_plugin/index.php
@@ -12,7 +12,26 @@ if(!defined('ABSPATH'))exit; //Exit if plugin is accessed directly
 class WordFilterPlugin{
     function __construct(){
         add_action('admin_menu', array($this, 'theMenu'));
-    }
+       if(get_option('pluginwordstofilter')) add_filter('the_content', array($this, 'filterLogic')
+    );
+add_action('admin_init', array($this, 'registerSettings'));
+}
+function registerSettings(){
+    add_settings_section('replacement_text_section', 'Replacement Text', null, 'wordfilteroptions' );
+    register_setting('replacementFields', 'replacementText');
+    add_settings_field('replacement-text', 'Filtered Words Replacement Text', array($this, 'replacementFieldHtml','wordfilteroptions', 'replacement_text_section' ));
+}
+function replacementFieldHtml(){ ?>
+<input type="text" name="replacementText" id="replacementText" value="<?php echo esc_attr(get_option('replacementText', '****')) ?>" />
+<p class="description">Leave blank to simply remove the filtered words.</p>
+<?php
+}
+function filterLogic($content){
+    $badWords = explode(',', get_option('pluginwordstofilter'));
+
+$badWordsTrimmed = array_map('trim', $badWords);
+return str_ireplace($badWordsTrimmed, esc_html(get_option('replacementText', '****')), $content);
+}
     function theMenu(){
         /*add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'wordfilter', array($this, 'wordFilterPage'), 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+CiA=', 110);*/
         $mainPageHook = add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'wordfilter', array($this, 'wordFilterPage'),plugin_dir_url(__FILE__).'custom.svg', 110);
@@ -23,8 +42,18 @@ class WordFilterPlugin{
     function mainPageAssets(){wp_enqueue_style('filterAdminCss', plugin_dir_url(__FILE__).'styles.css');
     }
     function subMenuOptions(){ ?>
+    <div class = "wrap">
     <h2>Word Filter Options</h2>
-<?php }
+    <form action="options.php" method="POST">
+        <?php
+        settings_fields('replacementFields');
+        do_settings_sections('wordfilteroptions');
+        submit_button();
+        ?>
+    </form>
+    </div>
+    <?php
+ }
 
 function handleForm(){
     if(wp_verify_nonce($_POST['theFilterNonce'], 'saveFilterWords') AND current_user_can('manage_options')){
@@ -44,7 +73,7 @@ function handleForm(){
              ?>
         <form method="POST">
             <input type="hidden" name="justsubmitted" class="justsubmitted" value=true>
-           <?php wp_nonce_field('saveFilterWords', 'theFilterNonce'); ?>
+           <?php wp_nonce_field('saveFilterWords', 'theFilterNonce'); ?>;
             <label for="plugin_wordfilter">
                 <p>Enter a <strong>comma-separated</strong> list of words to filter from your site content.</p>
             </label>
